7ca5a0ee118f9c7e9afe50e2fcd50770caea676f
Sierra Moore <sierra.v.moore@gmail.com>
2019-01-24 15:39:07 -0600

    created terinary displaytoggle for persons elements

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       { name: 'Roma', age: 4},
       { name: 'Felix', age: 14}
     ],
-      otherState: 'some other value'
+      otherState: 'some other value',
+      showPerson: false
   };
 
   switchNameHandler = (newName) => {
@@ -35,6 +36,11 @@ class App extends Component {
       })
   };
 
+  togglePersonHandler = () => {
+      const doesShow = this.state.showPerson; // current state(false)
+      this.setState({showPerson: !doesShow}); // change state to opposite of what current state is
+  };
+
   render() {
       // inline-styles will be scoped to component
       const btnStyle = {
@@ -50,21 +56,26 @@ class App extends Component {
         <h1>Im a react app</h1>
         <button
             style={btnStyle}
-            onClick={() => this.switchNameHandler('new name Sierra')}>Switch name</button>
-        <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-        />
-        <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Sierrachka!!')}
-        />
-        <Person name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-                changed={this.nameChangedHandler}>
-            My hobbies are: Watching the hobbit
-        </Person>
+            onClick={this.togglePersonHandler}>Display People</button>
+       {
+           this.state.showPerson ?
+           <div>
+              <Person
+                  name={this.state.persons[0].name}
+                  age={this.state.persons[0].age}
+              />
+              <Person
+                  name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  click={this.switchNameHandler.bind(this, 'Sierrachka!!')}
+              />
+              <Person name={this.state.persons[2].name}
+                      age={this.state.persons[2].age}
+                      changed={this.nameChangedHandler}>
+                  My hobbies are: Watching the hobbit
+              </Person>
+          </div> : null
+       }
       </div>
     );
   }
@@ -74,3 +85,6 @@ export default App;
 
 // in btn call an anyonmus function that returns switchNameHandler() function call..
 // when using arrow function syntax it implicity applies the return keyword directly after '=>' if its in one line if not in one line then wrap in curly braces
+
+// add a prop to determine true or false  (display person or not)
+// wrapped 'html Person(s)' in a div with a terinary if statement, if true display else null (dont show)
